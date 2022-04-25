10ef117d2e9866c13ebe9ea9462828e63d87209b
amanda hotchkiss <amanda.hotchkiss1011@gmail.com>
2022-04-25 17:52:26 -0400

    added display area and styling

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Create 4 operator functions:
+    // Add
+    // Subtract
+    // Divide
+    // Multiply
+
+// Create "operate" function w/ 3 parameters: 2 numbers and 1 operator
+
+// Create function that makes buttons display a value in the display area
