4311e1aedbf4d51f30942c1b33e964c6c82aad4d
amanda hotchkiss <amanda.hotchkiss1011@gmail.com>
2022-04-27 23:20:34 -0400

    began consolidating num and displayed variables

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ let plus = document.querySelector('.add');
 let minus = document.querySelector('.subtract');
 let divide = document.querySelector('.divide');
 let times = document.querySelector('.multiply');
-let equals = document.querySelector('.equals');
 let numberBtns = document.querySelectorAll('.number');
 let prevOperation = document.querySelector('.prev-operation');
 let currentOperation = document.querySelector('.current-operation');
@@ -10,64 +9,64 @@ let clearBtn = document.querySelector('.clear');
 let deleteBtn = document.querySelector('.delete');
 
 let operator = "";
-let num1 = [];
-let num2 = [];
+let currentOperationText = currentOperation.textContent;
+prevOperationText = prevOperation.textContent;
 
 clearBtn.addEventListener('click', function() {
     currentOperation.textContent = "";
     prevOperation.textContent = "";
-    num1 = [];
-    num2 = [];
-    return num1, num2
+    return currentOperationText, prevOperationText
 })
 
+// FIX THIS!!!
 deleteBtn.addEventListener('click', function() {
-    num1.pop();
-    currentOperation.textContent = num1.join("");
-    return num1;
+    currentOperation.pop();
+    return currentOperationText;
 })
 
-currentOperation.textContent = num1.join("");
 numberBtns.forEach(n => n.addEventListener('click', function () {
     if (operator === "") {
-        num1.push(n.textContent);
-        currentOperation.textContent = num1.join("");
+        console.log(currentOperationText)
+        currentOperationText = currentOperationText.toString() + n.textContent;
+        currentOperation.textContent = currentOperationText;
+        return currentOperationText;
     } else {
-        num2.push(n.textContent);
-        currentOperation.textContent = num2.join("");
+        prevOperationText = currentOperationText;
+        currentOperationText = currentOperationText + n.textContent;
+        return currentOperationText;
     }
 }))
 
 // Create 4 operator functions:
     // Add
-function Add() {
+const add = function() {
     operator = "+";
-    prevOperation.textContent = `${num1.join("")} ${operator}`;
+    prevOperationText = `${currentOperationText} ${operator}`;
     return operator;
 }
     // Subtract
-function Subtract() {
+const subtract = function () {
     operator = "-";
-    prevOperation.textContent = `${num1.join("")} ${operator}`;
+    prevOperationText = `${currentOperationText} ${operator}`;
     return operator;
 }
     // Divide
-function Divide() {
+const divided = function () {
     operator = "/";
     prevOperation.textContent = `${num1.join("")} ${operator}`;
     return operator;
 }
     // Multiply
-function Multiply() {
+const multiply = function () {
     operator = "*";
     prevOperation.textContent = `${num1.join("")} X `;
     return operator;
 }
 
-plus.addEventListener('click', Add);
-minus.addEventListener('click', Subtract);
-divide.addEventListener('click', Divide);
-times.addEventListener('click', Multiply);
+plus.addEventListener('click', add);
+minus.addEventListener('click', subtract);
+divide.addEventListener('click', divided);
+times.addEventListener('click', multiply);
 
 // Create "operate" function w/ 3 operators: 2 numbers and 1 operator
 function operate(num1, num2, operator) {
